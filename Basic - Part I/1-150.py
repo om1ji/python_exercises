29b660b432e72b2d1a7e4e72f5d91c1e1fc206c6
om1ji <om1ji.music@gmail.com>
2021-08-09 11:11:21 +0500

    33 to 66

--- Basic - Part I/1-150.py
+++ Basic - Part I/1-150.py
@@ -137,12 +137,11 @@ def twenty_eight(numbers: list):
             break
         elif i % 2 == 0: print(i)
 
-def twenty_nine(list1: set, list2: set): print(list1.difference(list2))
+def twenty_nine(list1: set, list2: set): 
+    print(list1.difference(list2))
 
-# color_list_1 = set(["White", "Black", "Red"])
-# color_list_2 = set(["Red", "Green"])
-
-def thirty(base, height): print(0.5*base*height)
+def thirty(base, height): 
+    print(0.5*base*height)
 
 def thirty_one(a: int, b: int):
     from math import gcd
@@ -152,6 +151,149 @@ def thirty_two(a: int, b:int):
     from math import gcd
     print(abs(a,b)//gcd(a,b))
 
-def thirty_three(a: int, b: int, c:int):
+def thirty_three(a: int, b: int, c: int):
     if len(set([a,b,c]))>3: print('0')
     else: print(a+b+c)
+
+def thirty_four(a: int, b:int):
+    if 15 <= (a + b) <= 20: print(20)
+    else: print(a + b)
+
+def thirty_five(a: int, b: int):
+    if a == b or a + b == 5 or b - a == 5: print(1)
+
+def thirty_six(a: int, b: int) -> int:
+    if isinstance(a, int) and isinstance(b, int): print(a+b)
+
+def thirty_seven(name, age, address): 
+    print(name, age, address, sep='\n')
+
+def thirty_eight(x: int, y: int) -> int: 
+    print((x+y)**2)
+
+def thirty_nine(amt, int, years):
+    r = amt*((1+(0.01*int)) ** years)
+    print(round(r, 2))
+
+def fourty(x1, y1, x2, y2): 
+    print(x2-x1, y2-y1, sep=' ')
+
+def fourty_one(path):
+    from os.path import exists
+    print(exists(path))
+
+def fourty_two():
+    import platform
+    print(platform.architecture()[0])
+
+def fourty_three():
+    import os, platform
+    print(os.name, platform.system(), platform.release(), sep='\n')
+
+def fourty_four():
+    import site
+    print(site.getsitepackages())
+
+def fourty_five(cmd):
+    import os
+    os.system(cmd)
+
+def fourty_six():
+    from os.path import realpath
+    print(realpath(__file__))
+
+def fourty_seven():
+    import multiprocessing as m
+    print(m.cpu_count())
+
+def fourty_eight(string, *args):
+    if 'float' in args: print(float(string))
+    elif 'integer' in args: print(int(string))
+
+def fourty_nine():
+    from os import listdir
+    print(listdir())
+
+def fifty():
+    pass
+
+def fifty_one(func: str):
+    import cProfile
+    cProfile.run(f'{func}')
+
+def fifty_two():
+    import sys
+
+    def eprint(*args, **kwargs):
+        print(*args, file=sys.stderr, **kwargs)
+
+    eprint("abc", "efg", "xyz", sep="--")
+
+def fifty_three(var_name):
+    from os import getenv
+    print(getenv(var_name).split(';'))
+
+def fifty_four():
+    from getpass import getuser
+    print(getuser())
+
+def fifty_five():
+    import socket # Не сам
+    print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
+    if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
+    s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+
+def fifty_six():
+    import fcntl, termios, struct # Не сам
+    th, tw = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+
+    print('Number of columns and Rows: ', tw, th, sep=' ')
+
+def fifty_seven():
+    from time import time
+    time1 = time()
+    bruh = 2**2**2**2**2
+    time2 = time()
+    print(bruh, time2-time1)
+
+def fifty_eight(n):
+    bruh = 0
+    for i in range(n):
+        bruh += i
+    print(bruh)
+
+def fifty_nine(foot, inches):
+    print(foot*30.48 + inches*2.54)
+
+def sixty(katet1, katet2):
+    from math import sqrt
+    hypotenuse = sqrt(katet1**2 + katet2**2)
+    print(round(hypotenuse, 2))
+
+def sixty_one(feet):
+    print(feet*12)
+    print(feet/3)
+    print(feet/5280)
+
+def sixty_two():
+    pass
+
+def sixty_three(path):
+    from os.path import abspath
+    print(abspath(path)) 
+
+def sixty_four():
+    pass
+
+def sixty_five(seconds: int):
+    days = seconds // (24*3600)
+    hours = (seconds // 3600) % 24
+    minutes = (seconds // 60) % 60
+    seconds = seconds % 60
+    print(days, hours, minutes, seconds, sep=' ')
+
+def sixty_six(kg, m: float):
+    print(round(kg/m**2, 1))
+
